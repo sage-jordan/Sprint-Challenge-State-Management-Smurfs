ed4db63132a9020cf1ea51a7edeb06df1d9d58ab
sage-jordan <sagemjrodan@gmail.com>
2019-09-29 21:49:04 -0400

    added redux-logger and fixed fetch/get error

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 export const GET_SMURFS_START = "GET_SMURFS_START";
-export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
-export const FETCH_SMURFS_FAILURE = 'FETCH_SMURFS_FAILURE';
+export const GET_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
+export const GET_SMURFS_FAILURE = 'FETCH_SMURFS_FAILURE';
 
 export const getSmurfs = () => dispatch => {
     dispatch({ type: GET_SMURFS_START });
     axios.post('http://localhost:3333/smurfs')
         .then(res => {
-            dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data });
+            dispatch({ type: GET_SMURFS_SUCCESS, payload: res.data });
         })
         .catch(err => {
-            dispatch({ type: FETCH_SMURFS_FAILURE, payload: `${err.response.status} ${err.response.data}`});
+            dispatch({ type: GET_SMURFS_FAILURE, payload: `${err.response.status} ${err.response.data}`});
         });
 }; 
