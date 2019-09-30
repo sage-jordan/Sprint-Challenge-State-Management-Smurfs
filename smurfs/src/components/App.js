65ea8d397238dc097132beb5b659a7ef8f6c6250
sage-jordan <sagemjrodan@gmail.com>
2019-09-29 22:14:17 -0400

    edited app to be a func and isEditing useState

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,30 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./App.css";
 import SmurfList from "./SmurfList";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Smurf Family!</h1>
-          <SmurfList />
-      </div>
-    );
-  }
+function App () {
+  const [isEditing, setIsEditing ] = useState(false);
+
+  const toggleIsEditing = e => {
+    e.preventDefault();
+    setIsEditing(!isEditing);
+  };
+
+  return (
+    <div className="App">
+      <h1>Smurf Family!</h1>
+        <SmurfList />
+        {!isEditing ? (
+          <button onClick={toggleIsEditing}>Add Smurf</button>
+        ) : (
+          <div>
+            <input 
+            
+            />
+          </div>
+        )}
+    </div>
+  );
 }
 
 export default App;
