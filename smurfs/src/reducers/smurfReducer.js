6917e6a6f3a8dc7023cbf84f557e6cf537246303
sage-jordan <sagemjrodan@gmail.com>
2019-09-29 21:35:33 -0400

    blank reducer

---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,12 @@
+import { type } from "os";
+
+export const initialValue = {
+
+};
+
+export const smurfReducer = (state = initialValue, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
