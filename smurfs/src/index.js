ef5be1bdc282487c1f33caa2044c96f7d8686f7b
sage-jordan <sagemjrodan@gmail.com>
2019-09-29 21:47:10 -0400

    added reducer to store and wrote cases and initVal

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -5,11 +5,11 @@ import App from "./components/App";
 
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-// import { dataReducer } from './reducers/dataReducer';
+import { smurfReducer } from './reducers/smurfReducer';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
-const store = createStore(applyMiddleware(thunk, logger));
+const store = createStore(smurfReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
     <Provider store={store}>
--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -1,11 +1,31 @@
-import { type } from "os";
+import { GET_SMURFS_START, GET_SMURFS_FAILURE, GET_SMURFS_SUCCESS } from "../actions";
 
 export const initialValue = {
-
+    smurfs: [],
+    error: '',
+    isFetching: false
 };
 
 export const smurfReducer = (state = initialValue, action) => {
     switch(action.type) {
+        case GET_SMURFS_START:
+            return {
+                ...state,
+                error: '',
+                isFetching: true
+            }
+        case GET_SMURFS_SUCCESS:
+            return {
+                ...state,
+                smurfs: action.payload,
+                isFetching: false
+            }
+        case GET_SMURFS_FAILURE:
+            return {
+                ...state,
+                error: action.payload,
+                isFetching: false
+            }
         default:
             return state;
     }
